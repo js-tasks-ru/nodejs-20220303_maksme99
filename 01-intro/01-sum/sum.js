f9689c27fc7ed2757c019215789598c967ac00c6
Maksim <maksme99@gmail.com>
2022-03-01 17:06:39 +0300

    Первая задача

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,11 @@
+const { isNumber } = require("lodash");
+
 function sum(a, b) {
   /* ваш код */
+  if(isNumber(a) && isNumber(b))
+  return a+b;
+  else
+  throw new TypeError();
 }
 
 module.exports = sum;
