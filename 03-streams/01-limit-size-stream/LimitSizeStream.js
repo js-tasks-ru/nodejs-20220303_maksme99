33d1795a29e177509cfaceaea4f04742fad3517c
Maksim <maksme99@gmail.com>
2022-03-12 21:11:45 +0300

    stream 1 task

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -1,13 +1,25 @@
+const { codeBlocks } = require('juice');
 const stream = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.size = 0;
+    this.max = options.limit;
   }
 
   _transform(chunk, encoding, callback) {
-
+    this.size +=  Buffer.byteLength(chunk);
+    if(this.size > this.max)
+    {
+      callback(new LimitExceededError());
+      //throw new LimitExceededError();
+    }
+    else
+    {
+      callback(null, chunk);
+    }
   }
 }
 
